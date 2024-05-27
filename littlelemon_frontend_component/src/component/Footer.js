abb02554de1ec430527eda9bc1af3830eeac01da
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-05-27 04:09:58 +0100

    changes: completed Footer component

--- littlelemon_frontend_component/src/component/Footer.js
+++ littlelemon_frontend_component/src/component/Footer.js
@@ -1,39 +1,79 @@
 import footlogo from '../llemon_asset/Asset 18@4x.png'
+import styled from 'styled-components'
 
 export default function Footer () {
+	const Footer = styled.footer`
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+		gap: 2%;
+	`
+	const GridContainer = styled.div`
+		display: grid;
+		grid-template-columns: repeat(3, 1fr);
+		// gap: 20px;
+		padding: 2.6%;
+
+		@media (max-width: 1000px) {
+			font-size: 0.875rem;
+			padding: 2.4%;
+		}
+		@media (max-width: 480px) {
+			font-size: 0.75rem;
+			padding: 2%;
+	`
+	const LogoImage = styled.img`
+		padding-top: 4%;
+		height: 130px;
+
+		@media (max-width: 1000px) {
+			padding-top: 3.5%;
+			height: 78px;
+		}
+		@media (max-width: 740px) {
+			padding-top: 4%;
+			// height: 78px;
+		}
+		@media (max-width: 480px) {
+			padding-top: 3%;
+			height: 48px;
+		}
+	`
 	return (
-        <footer className='footer'>
-			<img src={footlogo} alt="footer logo" />
-			<div className='footer-div'>
-				<div>
-					<p>
-						<span>Copyright © 2021</span><br></br>
-						<span>Lemon Restaurant</span><br></br>
-						<span>All rights reserved</span><br></br>
-						<span>Privacy Policy</span><br></br>
-						<span>Terms of Service</span><br></br>
-					</p>
-				</div>
-				<div>
-					<p>
-						<span>Social media:</span><br></br><br></br>
-						<span><a href='www.facebook.com'>facebook</a></span><br></br>
-						<span><a href='www.twitter.com'>twitter</a></span><br></br>
-						<span><a href='www.linkedin.com'>linkedin</a></span>
-					</p>
-				</div>
-				<div>
-					<p>
-						<span>Address:</span><br></br>
-						<span>123 Main Street City</span><br></br>
-						<span>State 12345</span><br></br>
-						<span>Phone: 123-456-7890</span><br></br>
-						<span>Fax: 123-456-7890</span><br></br>
-						<span>Email: 123-456-7890</span><br></br>
-						<span>Hours: 123-456-7890</span>
-					</p>
-                </div>
-			</div>
-		</footer>
+		<>
+			<Footer>
+				<LogoImage src={footlogo} alt="footer logo" />
+				<GridContainer>
+					<div>
+						<p>
+							<span>Copyright © 2021</span><br></br>
+							<span>Lemon Restaurant</span><br></br>
+							<span>All rights reserved</span><br></br>
+							<span>Privacy Policy</span><br></br>
+							<span>Terms of Service</span><br></br>
+						</p>
+					</div>
+					<div>
+						<p>
+							<span>Social media:</span><br></br><br></br>
+							<span><a href='www.facebook.com'>facebook</a></span><br></br>
+							<span><a href='www.twitter.com'>twitter</a></span><br></br>
+							<span><a href='www.linkedin.com'>linkedin</a></span>
+						</p>
+					</div>
+					<div>
+						<p>
+							<span>Address:</span><br></br>
+							<span>123 Main Street City</span><br></br>
+							<span>State 12345</span><br></br>
+							<span>Phone: 123-456-7890</span><br></br>
+							<span>Fax: 123-456-7890</span><br></br>
+							<span>Email: 123-456-7890</span><br></br>
+							<span>Hours: 123-456-7890</span>
+						</p>
+					</div>
+				</GridContainer>
+			</Footer>
+		</>
     );
 }
