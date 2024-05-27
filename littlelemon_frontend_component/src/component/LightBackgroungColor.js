cea1d209d8b8cf4cb67b204daf7eddee8f3d1cc6
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-05-27 04:12:55 +0100

    new (experimental): LightBackgroundColor

---
+++ littlelemon_frontend_component/src/component/LightBackgroungColor.js
@@ -0,0 +1,16 @@
+
+
+const LightBackgroundColor = () => {
+	const properties = {
+		backgroundColor: '#495E57',
+		height: 250,
+		position: 'relative',
+	};
+	return (
+		<div style={properties}>
+            <h1>Light Background Color</h1>
+        </div>
+	)
+}
+
+export default LightBackgroundColor;
