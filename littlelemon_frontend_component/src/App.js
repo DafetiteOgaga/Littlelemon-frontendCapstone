47e15c202827bf80f16b8bce9d5bb65b9e09ce52
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-05-27 04:15:55 +0100

    changes: added complonents and rendered them to the browser

--- littlelemon_frontend_component/src/App.js
+++ littlelemon_frontend_component/src/App.js
@@ -1,30 +1,62 @@
-// import logo from './logo.svg';
 import './App.css';
 import About from './component/About';
 import Navigation from './component/Navigation';
 import Testimonial from './component/Testimonial';
 import Footer from './component/Footer';
 import Header from './component/Header';
-import Hero from './component/Hero';
-import Highlights from './component/Highlights';
-import Main from './component/Main';
-
-// function App() {
+import Home from './component/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NotFound from './component/NotFound';
+import styled from 'styled-components'
 
 function App() {
+  const TopBarContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  // padding: 1% 10%;
+  padding-left: 10%;
+  padding-right: 10%;
+  // width: 100%;
+  // box-sizing: border-box;
+
+  @media (max-width: 1000px) {
+    flex-direction: column;
+    align-items: flex-end;
+    place-items: center;
+  }
+  @media (max-width: 480px) {
+      padding: 1% 3%;
+      font-size: 60%;
+    }
+  `
   return (
-    <>
-      <div className='header-plus'>
-        <Header />
-        <Navigation />
-      </div>
-      <Hero />
-      <Main />
-      <Highlights />
-      <Testimonial />
-      <About />
-      <Footer />
-    </>
+    <div style={{
+      marginTop: '2%',
+    }}>
+      <Router>
+        <TopBarContainer>
+          <Header />
+          <Navigation />
+        </TopBarContainer>
+        <main>
+        <div>
+          <Routes>
+              <Route path="/" element={<Home />} />
+              {/* <Route path="/contact" element={<Contact />} /> */}
+              <Route path="/about" element={<About />} />
+              {/* <Route path="/reservations" element={<Reservations />} /> */}
+              <Route path="/testimonial" element={<Testimonial />} />
+              {/* <Route path='/login' element={<Login />} /> */}
+              {/* <Route path='/logout' element={<Logout />} /> */}
+              {/* <Route path='/register' element={<Register />} /> */}
+              <Route path="*" element={<NotFound />} />
+          </Routes>
+          </div>
+          <Footer />
+        </main>
+      </Router>
+    </div>
   );
 }
 
