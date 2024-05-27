5900f403d634fd5105b601e6a89cf36c73c40a13
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-05-27 04:10:20 +0100

    changes: completed Header component

--- littlelemon_frontend_component/src/component/Header.js
+++ littlelemon_frontend_component/src/component/Header.js
@@ -1,10 +1,29 @@
 import logo from '../llemon_asset/Asset 16@4x.png'
+import styled from 'styled-components'
 
 export default function Header () {
+	const Header = styled.header`
+		min-height: 10vh;
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: center;
+		gap: 20px;
+
+		@media (max-width: 480px) {
+			min-height: 10vh;
+		}
+	`
+	const LogoImage = styled.img`
+		height: 60px;
+
+		@media (max-width: 480px) {
+			justify-content: center;
+		}
+	`
 	return (
-			<header className='header'>
-				<img src={logo} alt="page logo" />
-				{/* <h1>Little Lemon Restaurant</h1> */}
-			</header>
+			<Header>
+				<LogoImage src={logo} alt="page logo" />
+			</Header>
     )
 }
