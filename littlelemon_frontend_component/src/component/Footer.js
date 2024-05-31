487ead25eb08f5d62f031b0ac599e41fdbc8992c
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-05-31 15:38:51 +0100

    changes: minor fix

--- littlelemon_frontend_component/src/component/Footer.js
+++ littlelemon_frontend_component/src/component/Footer.js
@@ -1,47 +1,49 @@
 import footlogo from '../llemon_asset/Asset 18@4x.png'
 import styled from 'styled-components'
 
+// export default function Footer () {
+const FooterContainer = styled.footer`
+	display: flex;
+	flex-direction: row;
+	justify-content: center;
+	gap: 2%;
+`
+const GridContainer = styled.div`
+	display: grid;
+	grid-template-columns: repeat(3, 1fr);
+	// gap: 20px;
+	padding: 2.6%;
+
+	// @media (max-width: 1000px) {
+	// 	font-size: 0.875rem;
+	// 	padding: 2.4%;
+	// }
+	// @media (max-width: 480px) {
+	// 	font-size: 0.75rem;
+	// 	padding: 2%;
+	// }
+`
+const LogoImage = styled.img`
+	padding-top: 4%;
+	height: 130px;
+
+	// @media (max-width: 1000px) {
+	// 	padding-top: 3.5%;
+	// 	height: 78px;
+	// }
+	// @media (max-width: 740px) {
+	// 	padding-top: 4%;
+	// 	// height: 78px;
+	// }
+	// @media (max-width: 480px) {
+	// 	padding-top: 3%;
+	// 	height: 48px;
+	// }
+`
 export default function Footer () {
-	const Footer = styled.footer`
-		display: flex;
-		flex-direction: row;
-		justify-content: center;
-		gap: 2%;
-	`
-	const GridContainer = styled.div`
-		display: grid;
-		grid-template-columns: repeat(3, 1fr);
-		// gap: 20px;
-		padding: 2.6%;
-
-		@media (max-width: 1000px) {
-			font-size: 0.875rem;
-			padding: 2.4%;
-		}
-		@media (max-width: 480px) {
-			font-size: 0.75rem;
-			padding: 2%;
-	`
-	const LogoImage = styled.img`
-		padding-top: 4%;
-		height: 130px;
-
-		@media (max-width: 1000px) {
-			padding-top: 3.5%;
-			height: 78px;
-		}
-		@media (max-width: 740px) {
-			padding-top: 4%;
-			// height: 78px;
-		}
-		@media (max-width: 480px) {
-			padding-top: 3%;
-			height: 48px;
-		}
-	`
 	return (
 		<>
-			<Footer>
+			<FooterContainer>
 				<LogoImage src={footlogo} alt="footer logo" />
 				<GridContainer>
 					<div>
@@ -73,7 +75,7 @@ export default function Footer () {
 						</p>
 					</div>
 				</GridContainer>
-			</Footer>
+			</FooterContainer>
 		</>
     );
 }
