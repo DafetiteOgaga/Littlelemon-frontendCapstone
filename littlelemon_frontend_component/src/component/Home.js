176e6fd3ca8dbf883882ea874dc2ce5253210dbd
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-05-31 15:39:30 +0100

    changes: minor fixes

--- littlelemon_frontend_component/src/component/Home.js
+++ littlelemon_frontend_component/src/component/Home.js
@@ -2,6 +2,8 @@ import Hero from "./Hero"
 import Highlights from "./Highlights"
 import Testimonial from "./Testimonial"
 import About from "./About"
+// import { useState } from "react"
+// import BookingPage from "./BookingPage"
 
 export default function Home () {
 	return (
