9c6f65c623a6e81fc7ca767a2d2ec463babfbdff
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-05-31 15:43:02 +0100

    implemented more components and rendered them here

--- littlelemon_frontend_component/src/App.js
+++ littlelemon_frontend_component/src/App.js
@@ -3,60 +3,65 @@ import About from './component/About';
 import Navigation from './component/Navigation';
 import Testimonial from './component/Testimonial';
 import Footer from './component/Footer';
-import Header from './component/Header';
+import Logo from './component/Logo';
 import Home from './component/Home';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NotFound from './component/NotFound';
 import styled from 'styled-components'
+import BookingPage from './component/BookingPage';
+// import BookingForm from './component/BookingForm';
+import ConfirmedBooking from './component/ConfirmedBooking';
 
-function App() {
-  const TopBarContainer = styled.div`
+const MainContainer = styled.main`
   display: flex;
-  align-items: center;
-  justify-content: space-between;
-  // padding: 1% 10%;
-  padding-left: 10%;
-  padding-right: 10%;
-  // width: 100%;
-  // box-sizing: border-box;
+  flex-direction: column;
 
-  @media (max-width: 1000px) {
-    flex-direction: column;
-    align-items: flex-end;
-    place-items: center;
+  // ull viewport height
+  height: 100vh;
+`
+const HeaderContainer = styled.header`
+  flex: 1 1 auto;
+  display: grid;
+  align-items: center;
+  grid-template-columns: 1fr 1fr;
+  margin-top: 2%;
+
+  // @media (max-width: 1000px) {
+  //   flex-direction: column;
+  //   align-items: flex-end;
+  //   place-items: center;
+  // }
+  // @media (max-width: 480px) {
+  //     padding: 1% 3%;
+  //     font-size: 60%;
   }
-  @media (max-width: 480px) {
-      padding: 1% 3%;
-      font-size: 60%;
-    }
-  `
+`
+const BodyContainer = styled.div`
+  flex: 1 1 auto;
+`
+function App() {
   return (
-    <div style={{
-      marginTop: '2%',
-    }}>
+    <MainContainer>
       <Router>
-        <TopBarContainer>
-          <Header />
+        <HeaderContainer className='App'>
+          <Logo />
           <Navigation />
-        </TopBarContainer>
-        <main>
-        <div>
+        </HeaderContainer>
+        <BodyContainer>
           <Routes>
-              <Route path="/" element={<Home />} />
-              {/* <Route path="/contact" element={<Contact />} /> */}
-              <Route path="/about" element={<About />} />
-              {/* <Route path="/reservations" element={<Reservations />} /> */}
-              <Route path="/testimonial" element={<Testimonial />} />
-              {/* <Route path='/login' element={<Login />} /> */}
-              {/* <Route path='/logout' element={<Logout />} /> */}
-              {/* <Route path='/register' element={<Register />} /> */}
-              <Route path="*" element={<NotFound />} />
+            <Route path="/" element={<Home />} />
+            {/* <Route path="/contact" element={<Contact />} /> */}
+            <Route path="/booking-page" element={<BookingPage/>} />
+            <Route path="/about" element={<About />} />
+            <Route path="/testimonial" element={<Testimonial />} />
+            <Route path="/confirmed" element={<ConfirmedBooking />}></Route>
+            {/* <Route path="/BookingForm" element={<BookingForm />}></Route> */}
+            <Route path="*" element={<NotFound />} />
           </Routes>
-          </div>
-          <Footer />
-        </main>
+        </BodyContainer>
+        <Footer styled={{flex: `1 1 auto`}} className='App'/>
       </Router>
-    </div>
+    </MainContainer>
   );
 }
 
