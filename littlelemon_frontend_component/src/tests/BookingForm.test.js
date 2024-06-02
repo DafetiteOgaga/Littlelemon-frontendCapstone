a83d98173a7c08fe1e32f43ec12fb94d60097bfe
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-06-02 01:39:42 +0100

    Renders the BookingForm heading

--- littlelemon_frontend_component/src/tests/BookingForm.test.js
+++ littlelemon_frontend_component/src/tests/BookingForm.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen } from './test-utils';
 import BookingForm from '../component/BookingForm';
 
 test('Renders the BookingForm heading', () => {
     render(<BookingForm />);
-    const headingElement = screen.getByText("Book Now");
+    // Use regex for case-insensitive match
+    const headingElement = screen.getByText(/First:/i);
     expect(headingElement).toBeInTheDocument();
-})
+});
