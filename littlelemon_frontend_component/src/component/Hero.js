c7043f52a3e5fa9b9a6c3c560e32f6ab12323267
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-06-02 01:47:19 +0100

    changes: improved user experience when rendering

--- littlelemon_frontend_component/src/component/Hero.js
+++ littlelemon_frontend_component/src/component/Hero.js
@@ -23,87 +23,17 @@ const specials = {
 
 const Section = styled.section``
 const GridContainer = styled.div`
+	flex: ${ ({$booking}) => $booking ? 'none' : '1 1 auto'};
 	display: grid;
 	grid-template-columns: repeat(2, 1fr);
 	gap: 2%;
+	margin-top: -2%;
 	margin-bottom: -20%;
-
-	// @media (max-width: 1310px) {
-	// 	margin-bottom: -19%;
-	// }
-	// @media (max-width: 1259px) {
-	// 	margin-bottom: -18%;
-	// }
-	// @media (max-width: 1223px) {
-	// 	margin-bottom: -13%;
-	// }
-	// @media (max-width: 1110px) {
-	// 	margin-bottom: -11%;
-	// }
-	// @media (max-width: 1058px) {
-	// 	margin-bottom: -6%;
-	// }
-	// @media (max-width: 1000px) {
-	// 	margin-bottom: -11%;
-	// }
-	// @media (max-width: 1000px) {
-	// 	margin-bottom: -8%;
-	// }
-	// @media (max-width: 886px) {
-	// 	margin-bottom: -4%;
-	// }
-	// @media (max-width: 817px) {
-	// 	margin-bottom: -2%;
-	// }
-	// @media (max-width: 740px) {
-	// 	margin-bottom: -8%;
-	// }
-	// @media (max-width: 533px) {
-	// 	margin-bottom: -6%;
-	// }
 `
 const Article = styled.article`
-	// max-width: 600%;
-	// margin: 0 auto;
+	flex: ${ ({$booking}) => $booking ? 'none' : '1 1 auto'};
 `
-const Image = styled.img`
-	width: 70%;
-	height: 77%;
-	border-radius: 10%;
-
-	// @media (max-width: 1300px) {
-	// 	// width: 100%;
-	// 	// height: 120%;
-	// }
-	// @media (max-width: 1221px) {
-	// 	height: 80%;
-	// 	// width: 45%;
-	// }
-	// @media (max-width: 1110px) {
-	// 	height: 80%;
-	// }
-	// @media (max-width: 1058px) {
-	// 	height: 90%;
-	// }
-	// @media (max-width: 1000px) {
-	// 	width: 70%;
-	// 	// height: 85%;
-	// }
-	// @media (max-width: 886px) {
-	// 	height: 100%;
-	// }
-	// @media (max-width: 740px) {
-	// 	height: 90%;
-	// }
-	// @media (max-width: 533px) {
-	// 	height: 95%;
-	// 	// border-radius: 10%;
-	// }
-	// @media (max-width: 480px) {
-	// 	height: 110%;
-	// 	// border-radius: 10%;
-	// }
-`
+const Image = styled.img``
 const CenterImage = styled.div`
 	display: flex;
 	justify-content: ${ ({$booking}) => $booking ? 'center' : 'end' };
@@ -116,188 +46,84 @@ const Head1 = styled.h1`
 	padding-bottom: ${ ({$booking}) => $booking ? '4.5%' : '0' };
 	padding-top: ${ ({$booking}) => $booking ? '2%' : '0' };
 	margin-bottom: 0;
-	margin-top: 0;
 
-	// // @media (max-width: 1000px) {
-	// // 	font-size: 250%;
-	// // }
-	// @media (max-width: 740px) {
-	// 	font-size: 140%;
-	// }
-	// @media (max-width: 665px) {
-	// 	font-size: 120%;
-	// }
-	// @media (max-width: 583px) {
-	// 	font-size: 100%;
-	// }
-	// @media (max-width: 480px) {
-	// 	font-size: 110%;
-	// }
-	// @media (max-width: 450px) {
-	// 	font-size: 95%;
-	// }
+	@media (max-width: 1000px) {
+		font-size: ${ ({$booking}) => $booking ? '240%' : '175%' };
+	}
+	@media (max-width: 740px) {
+		font-size: ${ ({$booking}) => $booking ? '240%' : '140%' };
+	}
+	@media (max-width: 580px) {
+		font-size: ${ ({$booking}) => $booking ? '240%' : '120%' };
+	}
+	@media (max-width: 520px) {
+		font-size: ${ ({$booking}) => $booking ? '240%' : '90%' };
+	}
+	@media (max-width: 480px) {
+		font-size: ${ ({$booking}) => $booking ? '240%' : '80%' };
+	}
 `
 const Head2 = styled.h2`
 	font-size: 130%;
-	margin-top: 0;
 	margin-bottom: ${ ({$booking}) => $booking ? '0' : '1' };
 	color: white;
 
-	// // @media (max-width: 1000px) {
-	// // 	font-size: 130%;
-	// // }
-	// @media (max-width: 740px) {
-	// 	font-size: 100%;
-	// }
-	// @media (max-width: 665px) {
-	// 	font-size: 85%;
-	// }
-	// @media (max-width: 583px) {
-	// 	font-size: 70%;
-	// }
-	// @media (max-width: 480px) {
-	// 	font-size: 70%;
-	// }
-	// @media (max-width: 450px) {
-	// 	font-size: 60%;
-	// }
+	@media (max-width: 1000px) {
+		font-size: 120%;
+	}
+	@media (max-width: 740px) {
+		font-size: 100%;
+	}
+	@media (max-width: 580px) {
+		font-size: 80%;
+	}
+	@media (max-width: 550px) {
+		font-size: 80%;
+	}
+	@media (max-width: 480px) {
+		font-size: 70%;
+	}
 `
 const Paragraph = styled.p`
-	font-size: 110%;
 	color: white;
-
-	// // @media (max-width: 1310px) {
-	// // 	font-size: 90%;
-	// // }
-	// @media (max-width: 740px) {
-	// 	font-size: 75%;
-	// }
-	// @media (max-width: 755px) {
-	// 	font-size: 75%;
-	// }
-	// @media (max-width: 655px) {
-	// 	font-size: 70%;
-	// }
-	// @media (max-width: 629px) {
-	// 	font-size: 65%;
-	// }
-	// @media (max-width: 583px) {
-	// 	font-size: 58%;
-	// }
-	// @media (max-width: 513px) {
-	// 	font-size: 53%;
-	// }
-	// @media (max-width: 480px) {
-	// 	font-size: 55%;
-	// }
-	// @media (max-width: 450px) {
-	// 	font-size: 45%;
-	// }
-	// @media (max-width: 370px) {
-	// 	font-size: 40%;
-	// }
 `
 const ButtonBox = styled.div`
-	background-color: #F4CE14;
 	height: 8%;
 	width: ${ ({$booking}) => $booking ? '12%' : '45%' };
 	padding-top: ${ ({$booking}) => $booking ? '1.2%' : '0%' };
 	padding-bottom: ${ ({$booking}) => $booking ? '1.2%' : '0%' };
 	margin-left: ${ ({$booking}) => $booking ? '10%' : '0%' };
-	border-radius: 30px;
-	border-style: hidden;
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	cursor: pointer;
-
-	// @media (max-width: 1221px) {
-	// 	height: 9%;
-	// 	width: 45%;
-	// }
-	// @media (max-width: 1000px) {
-	// 	height: 8%;
-	// }
-	// @media (max-width: 823px) {
-	// 	height: 11%;
-	// }
-	// @media (max-width: 740px) {
-	// 	height: 8%;
-	// 	width: 40%;
-	// }
-	// @media (max-width: 613px) {
-	// 	height: 9%;
-	// 	width: 50%;
-	// }
-	// @media (max-width: 480px) {
-	// 	height: 10%;
-	// }
-
-	&:hover {
-		background-color: #fddd50;
-	}
-	&:active {
-		background-color: #6e6a52;
-	}
 `
-const Button = styled.button`
-	background-color: transparent;
-	font-weight: 900;
-	border-style: hidden;
-	font-size: 130%;
-	text-align: center;
-	display: inline-flex;
-	cursor: pointer;
-
-	// @media (max-width: 1000px) {
-	// 	font-size: 85%;
-	// }
-	// @media (max-width: 740px) {
-	// 	font-size: 70%;
-	// }
-	// @media (max-width: 480px) {
-	// 	font-size: 50%;
-	// }
-
-	&:active {
-		color: white
-	}
-`
+const Button = styled.button``
 const Background = styled.div`
-	background-color: #495E57;
-	// height: auto;
-	// min-height: 10vh; /* Default minimum height */
 	padding-top: ${ ({$booking}) => $booking ? '1.5%' : '5%' };
-	padding-bottom: 2%;
-	// padding-left: 10%;
-	// padding-right: 10%;
 	display: ${ ({$booking}) => $booking ? 'grid' : 'flex'};
-	justify-content: center;
+	margin-bottom: 6%;
 
-	// @media (max-width: 1000px) {
-	// 	padding-bottom:0;
-	// 	// padding-left: 10%;
-	// 	// padding-right: 10%;
-	// }
-	// @media (max-width: 887px) {
-	// 	// padding-bottom:4%;
-	// }
-	// @media (max-width: 825px) {
-	// 	// padding-bottom:8%;
-	// }
-	// @media (max-width: 740px) {
-	// 	padding-top: 4%;
-	// 	padding-bottom: 0;
-	// }
-	// @media (max-width: 655px) {
-	// 	// padding-bottom: 3%;
-	// }
-	// @media (max-width: 480px) {
-	// 	padding-top: 4%;
-	// 	padding-bottom: 4%;
-	// 	// padding-left: 10%;
-	// 	// padding-right: 10%;
-	// }
+	@media (max-width: 1266px) {
+		padding-bottom: 4%;
+	}
+	@media (max-width: 1152px) {
+		padding-bottom: 6%;
+	}
+	@media (max-width: 1058px) {
+		padding-bottom: 8%;
+	}
+	@media (max-width: 1000px) {
+		padding-bottom: 4%;
+	}
+	@media (max-width: 914px) {
+		padding-bottom: 6%;
+	}
+	@media (max-width: 872px) {
+		padding-bottom: 8%;
+	}
+	@media (max-width: 678px) {
+		padding-bottom: 10%;
+	}
+	@media (max-width: 580px) {
+		padding-bottom: 5%;
+	}
 `
 export default function Hero (props) {
 	const navigate = useNavigate();
@@ -305,29 +131,32 @@ export default function Hero (props) {
 	return (
 		<>
 			<Section>
-				<Background className='App' $booking={props.booking} >{ props.booking ? (
+				<Background className='App main-background' $booking={props.booking} >{ props.booking ? (
 					<Article >
-						<Head1 $booking={props.booking}
+						<Head1 className="head1" $booking={props.booking}
 						>{specials.bookingTitie}</Head1>
 					</Article>
 					) : (null)}
 					<GridContainer>
 						{ props.booking ? (
 							<CenterImage $booking={props.booking}>
-								<Image src={hero1} alt="hero image 1"/>
+								<Image className="form-image" src={hero1} alt="hero image 1"/>
 							</CenterImage>
 							) : (
 							<Article>
-								<Head1>Little Lemon</Head1>
-								<Head2>Chicago</Head2>
-								<Paragraph>{specials.description}</Paragraph>
-								<ButtonBox $booking={props.booking}>
-									<Button onClick={submitForm}>Reserve a table</Button>
+								<Head1 className="head1" >Little Lemon</Head1>
+								<Head2 className="head2" >Chicago</Head2>
+								<Paragraph className="paragraph">{specials.description}</Paragraph>
+								<ButtonBox
+									className="botton-box"
+									$booking={props.booking}
+									>
+										<Button onClick={submitForm}>Reserve a table</Button>
 								</ButtonBox>
 							</Article>
 						)}
 						<CenterImage $booking={props.booking}>
-							<Image src={specials.image} alt="hero image 2"/>
+							<Image className="hero-image" src={specials.image} alt="hero image 2"/>
 						</CenterImage>
 					</GridContainer>
 				</Background>
