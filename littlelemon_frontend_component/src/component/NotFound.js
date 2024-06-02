3d99a907d1494c7417503d45c4b6c4e34f479acb
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-06-02 01:49:57 +0100

    changes: added better visuals

--- littlelemon_frontend_component/src/component/NotFound.js
+++ littlelemon_frontend_component/src/component/NotFound.js
@@ -3,41 +3,12 @@ import styled from 'styled-components';
 const Background = styled.div`
 	background-color: #495E57;
 	color: white;
-	// color: #F4CE14;
-	border-radius: 2%;
-	// height: auto;
-	// min-height: 10vh; /* Default minimum height */
-	padding-top: 1%;
-	padding-bottom: 2%;
+	border-radius: 1rem;
+	padding: 8%;
 	display: flex;
     justify-content: center;
     align-items: center;
     flex-direction: column;
-
-	// @media (max-width: 1000px) {
-	// 	padding-bottom:0;
-	// 	// padding-left: 10%;
-	// 	// padding-right: 10%;
-	// }
-	// @media (max-width: 887px) {
-	// 	// padding-bottom:4%;
-	// }
-	// @media (max-width: 825px) {
-	// 	// padding-bottom:8%;
-	// }
-	// @media (max-width: 740px) {
-	// 	padding-top: 4%;
-	// 	padding-bottom: 0;
-	// }
-	// @media (max-width: 655px) {
-	// 	// padding-bottom: 3%;
-	// }
-	// @media (max-width: 480px) {
-	// 	padding-top: 4%;
-	// 	padding-bottom: 4%;
-	// 	// padding-left: 10%;
-	// 	// padding-right: 10%;
-	// }
 `
 const SpanText = styled.span`
 	color: red;
@@ -46,8 +17,8 @@ const NotFound = () => {
 	return (
         <div className='App'>
             <Background>
-                <h1><SpanText>Oopsy! 404</SpanText></h1>
-                <h3>Page Not Found</h3>
+                <h1 className='head1'><SpanText>Oopsy! 404</SpanText></h1>
+                <h2 style={{ color: 'white'}} className='head2'>Page Not Found</h2>
             </Background>
         </div>
     );
