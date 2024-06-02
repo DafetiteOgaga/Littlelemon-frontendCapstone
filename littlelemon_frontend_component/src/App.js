256b3236fe4a2ddff91ea2a7f0ac6e4ac14794f3
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-06-02 01:53:24 +0100

    changes: this component is completed and works as expected

--- littlelemon_frontend_component/src/App.js
+++ littlelemon_frontend_component/src/App.js
@@ -3,42 +3,56 @@ import About from './component/About';
 import Navigation from './component/Navigation';
 import Testimonial from './component/Testimonial';
 import Footer from './component/Footer';
-import Logo from './component/Logo';
-import Home from './component/Home';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import NotFound from './component/NotFound';
-import styled from 'styled-components'
 import BookingPage from './component/BookingPage';
-// import BookingForm from './component/BookingForm';
+import NotFound from './component/NotFound';
+import Home from './component/Home';
 import ConfirmedBooking from './component/ConfirmedBooking';
+import Logo from './component/Logo';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import styled from 'styled-components'
+
+// import BookingForm from './component/BookingForm';
+// import Hero from './component/Hero';
+// import Highlights from './component/Highlights';
+
 
 const MainContainer = styled.main`
   display: flex;
   flex-direction: column;
 
-  // ull viewport height
-  height: 100vh;
+  // all viewport height
+  // height: 100vh;
 `
 const HeaderContainer = styled.header`
-  flex: 1 1 auto;
-  display: grid;
+  // display: grid;
+  display: flex;
+  justify-content: space-between;
   align-items: center;
-  grid-template-columns: 1fr 1fr;
+  // grid-template-columns: 1fr 1fr;
   margin-top: 2%;
 
-  // @media (max-width: 1000px) {
-  //   flex-direction: column;
-  //   align-items: flex-end;
-  //   place-items: center;
-  // }
-  // @media (max-width: 480px) {
-  //     padding: 1% 3%;
-  //     font-size: 60%;
+  @media (max-width: 1000px) {
+    flex-direction: column;
+    align-items: flex-end;
+    place-items: center;
+  }
+  @media (max-width: 480px) {
+      padding: 1% 3%;
+      font-size: 60%;
   }
 `
-const BodyContainer = styled.div`
-  flex: 1 1 auto;
-`
+
+// const HEaders = () => {
+//   return (
+//     <>
+//       <Logo />
+//       <Navigation />
+//     </>
+//   )
+// }
+
+const BodyContainer = styled.div``
 function App() {
   return (
     <MainContainer>
@@ -54,12 +68,18 @@ function App() {
             <Route path="/booking-page" element={<BookingPage/>} />
             <Route path="/about" element={<About />} />
             <Route path="/testimonial" element={<Testimonial />} />
-            <Route path="/confirmed" element={<ConfirmedBooking />}></Route>
-            {/* <Route path="/BookingForm" element={<BookingForm />}></Route> */}
+            <Route path="/confirmed" element={<ConfirmedBooking />} />
             <Route path="*" element={<NotFound />} />
+
+            {/* <Route path="/BookingForm" element={<BookingForm />}></Route> */}
+            {/* <Route path="/header" element={<HEaders/>} /> */}
+            {/* <Route path="/booking-form" element={<BookingForm />} /> */}
+            {/* <Route path="/hero" element={<Hero />} /> */}
+            {/* <Route path="/highlights" element={<Highlights />} /> */}
+
           </Routes>
         </BodyContainer>
-        <Footer styled={{flex: `1 1 auto`}} className='App'/>
+        <Footer className='App'/>
       </Router>
     </MainContainer>
   );
