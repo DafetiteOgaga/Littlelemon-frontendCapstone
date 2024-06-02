cc732ba656db1253356d4aa4ccac210ebc2bccd9
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-06-02 01:48:45 +0100

    changes: improved responsiveness

--- littlelemon_frontend_component/src/component/Logo.js
+++ littlelemon_frontend_component/src/component/Logo.js
@@ -10,16 +10,22 @@ const HeaderContainer = styled.div`
 	// justify-content: center;
 	gap: 20px;
 
-	// @media (max-width: 480px) {
-	// 	min-height: 10vh;
-	// }
+	@media (max-width: 480px) {
+		min-height: 10vh;
+	}
 `
 const LogoImage = styled.img`
 	height: 60px;
 
-	// @media (max-width: 480px) {
-	// 	justify-content: center;
-	// }
+	@media (max-width: 1000px) {
+		height: 50px;
+	}
+	@media (max-width: 740px) {
+		height: 45px;
+	}
+	@media (max-width: 480px) {
+		height: 40px;
+	}
 `
 export default function Logo () {
 	return (
